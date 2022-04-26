12006fab317fee856da9e5bbe0ee1068d3c8aa04
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-26 16:01:24 -0700

    create component files

---
+++ src/components/Animal/Animal.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Animal.css';
+
+export default function Animal() {
+  return (
+    <div>Header</div>
+  );
+}
---
+++ src/components/AnimalList/AnimalList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './AnimalList.css';
+
+export default function AnimalList() {
+  return (
+    <div>Header</div>
+  );
+}
